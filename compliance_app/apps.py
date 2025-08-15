4855e2e80e8468356b5ad01068a0c80c1abe3ab5
usernamewolffe <andrew.peat@franklinbutler.com>
2025-08-15 23:38:48 +0100

    Delete

--- compliance_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComplianceAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'compliance_app'
--- config/settings/local.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-DEBUG = True
-ALLOWED_HOSTS = ["*"]
-
-INSTALLED_APPS = list(INSTALLED_APPS) + [
-    "incidents",
-]
-
